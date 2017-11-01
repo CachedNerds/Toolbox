7de49c5d64a3a74471a0cd31f2e271bb06825365
Damon Myers <damon.shane.myers@gmail.com>
2017-10-31 18:40:27 -0700

    Fix GCC compile errors. Use a c-style string for strftime

--- C++/libs/Time/src/Time.cpp
+++ C++/libs/Time/src/Time.cpp
@@ -17,7 +17,7 @@ const std::string getCurrentTime (void)
 {
   std::time_t now = std::chrono::system_clock::to_time_t (std::chrono::system_clock::now ());
   std::tm localNow;
-#ifdef _WIN32
+#if (_MSC_VER > 1300)
   // localtime_s is not in namespace std on vc++ libraries for some reason
   localtime_s(&localNow, &now);
 #else
@@ -26,9 +26,9 @@ const std::string getCurrentTime (void)
 
   constexpr int dateSize = 26;
   char format[] = "%c";
-  std::string timestamp{dateSize};
-  std::strftime(timestamp.data(), dateSize, format, &localNow);
-  return removeTrailingNewline (timestamp);
+  char timestamp[dateSize];
+  std::strftime(timestamp, dateSize, format, &localNow);
+  return removeTrailingNewline (std::string(timestamp));
 }
 
 } // namespace Toolbox::Time
